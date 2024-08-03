48f43d5aa00e469fe0583a2ae7aafdc530388167
cyrildmoses <cyrild.moses@gmail.com>
2024-08-03 03:25:26 -0400

    fix new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,20 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "cyrild.moses+workload_dev@gmail.com"
-    AccountName               = "workload-dev"
+    AccountEmail              = "cyrild.moses+workload-dev@gmail.com"
+    AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "workload"
-    SSOUserEmail              = "cyrild.moses+workload_dev@gmail.com"
-    SSOUserFirstName          = "cyril moses"
+    SSOUserEmail              = "cyrild.moses+workload-dev@gmail.com"
+    SSOUserFirstName          = "cyril"
     SSOUserLastName           = "devadass"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "description" = "My First Account"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "Cyril"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
