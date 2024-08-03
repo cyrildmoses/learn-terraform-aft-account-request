3e9ed3234726ea9265b60c6bcbfd0b13296eeb56
cyrildmoses <cyrild.moses@gmail.com>
2024-08-03 03:51:09 -0400

    workload-prod-account configuration

---
+++ terraform/workload-prod-account.tf
@@ -0,0 +1,26 @@
+module "workload-prod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "cyrild.moses+workload-prod@gmail.com"
+    AccountName               = "workload-prod"
+    ManagedOrganizationalUnit = "workload"
+    SSOUserEmail              = "cyrild.moses+workload-prod@gmail.com"
+    SSOUserFirstName          = "cyril"
+    SSOUserLastName           = "devadass"
+  }
+
+  account_tags = {
+    "description" = "My First Account"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Cyril"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+}
